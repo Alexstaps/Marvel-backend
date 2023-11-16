7c3f00b165cd1a93cf93c5fa96ee1821a42e71d2
Alexandre Houcine <alexandrehoucine@mbpdealexandre.lan>
2023-11-16 13:39:51 +0100

    import de cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const charactersRoutes = require("./routes/characters");
 const characterRoutes = require("./routes/character");
